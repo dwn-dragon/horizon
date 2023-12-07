816ac7b77cbd613ba76dc019013bc0eaafbefcbb
Dragon <christianlonghi@outlook.it>
2023-12-07 10:00:53 +0100

    Basic rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <thread>
 
-#include <GL/gl3w.h>
-#include <GLFW/glfw3.h>
+#include "gfx/core.h"
+#include "gfx/shaders.h"
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+constexpr float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
 
 int main(int argc, char const *argv[]) {
 	
@@ -47,12 +50,40 @@ int main(int argc, char const *argv[]) {
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
+	//	Shaders
+
+	gfx::Shader shader;
+
+	shader = gfx::Shader{ "shaders/vertex.shader.glsl", "shaders/fragment.shader.glsl" };
+
+	//	Model
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0));
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+
 	//	Main Loop
 
+	shader.use();
+	glBindVertexArray(VAO);
+
 	while(!glfwWindowShouldClose(window)) {
+		std::this_thread::sleep_for( std::chrono::duration<double, std::milli>(0.02f) );
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();    
